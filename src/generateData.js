f91711dee170a5f1f83add76972f2e6ceba1d580
cthutqspb <19940514@inbox.ru>
2026-05-08 11:45:39 +0300

    + длинные названия для теста

--- src/generateData.js
+++ src/generateData.js
@@ -7,8 +7,26 @@ function getRandomInt(min, max) {
 const generateShops = () => {
     const shops = [];
     //токионайт пушка
-    const colors = [0x7aa2f7, 0x2ac3de, 0x9ece6a, 0xbb9af7, 0xf7768e, 0xe0af68, 0x565f89]
-    const names = ['Wildberries', 'OZON', 'Авито', 'Яндекс маркет', 'Химчистка', 'Burger King', 'Гнилое яблоко']
+    const colors = [
+        0x7aa2f7,
+        0x2ac3de,
+        0x9ece6a,
+        0xbb9af7,
+        0xf7768e,
+        0xe0af68,
+        0x565f89
+    ]
+    
+    const names = [
+        `Wildberries`,
+        `OZON`,
+        `Авито`,
+        `Яндекс маркет`,
+        `Химчистка`,
+        `Burger King`,
+        `Гнилое яблоко`,
+        `Магазин c очень длинным название пря очень длинним аж не влезает а надо что влезал`
+    ]
 
     for (let i = 1; i <= 100; i++ ) {
         shops.push({
